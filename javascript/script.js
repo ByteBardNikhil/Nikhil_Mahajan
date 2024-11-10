ff19d8f9351564d6dab22c435e32b8e04927c68b
Nikhil-197 <mahajannikhil177@gmail.com>
2024-11-10 17:30:38 +0530

    Navbar Flips

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.querySelector(".menu-icon");
   const navList = document.getElementById("navlist");
+  const li = document.querySelector("#navlist li");
+
   menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("active");
+    if (menuIcon.classList.contains("active")) {
+      document.querySelector(".logo").style.display = "none";
+      navList.style.display = "flex";
+      li.forEach((ele) => (ele.style.fontSize = "0.3rem"));
+    } else {
+      document.querySelector(".logo").style.display = "flex";
+      navList.style.display = "none";
+    }
+
     navList.classList.toggle("active");
   });
 });
